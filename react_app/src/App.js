46ebac46625510b8fc96d2b2677af828e99fd76b
MissionaryPS <39906446+MissionaryPS@users.noreply.github.com>
2023-01-20 17:35:05 +0900

    3-3 p.164-177 イベントのバインド，プロパティとステートの連携？
    本ではthis.stateにデータを入れたあとthis.state.xxxで呼び出してないのだが．．．

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -2,33 +2,72 @@ import React, {Component} from 'react'
 import './App.css';
 
 class App extends Component {
+  
+  data = [
+    "this is list sample",
+    "これはリストのサンプルです．",
+    "配列をリストに変換します．"
+  ]
 
   constructor(props){
     super(props)
     this.state = {
-      msg:'Hello Component.',
-      count:0
+      list:this.data
     }
-    //let timerを消しても動く．
-    //setIntervalの引数は(関数，ミリ秒)だが，関数の宣言にアロー関数を用いている
-    let timer = setInterval(()=>{
-      this.setState({
-        count:this.state.count + 1,
-        msg:"[count:" + this.state.count + "]"
-      })
-    }, 1000)
+  }
+
+  doAction(e){
   }
 
   render(){ 
     return <div>
       <h1 className='bg-primary text-white display-4'>React</h1>
       <div className='container'>
-        <p className='subtitle'>Count number.</p>
-        <p className='alert alert-warning'>{this.state.msg}</p>
-        <p className='alert alert-dark'>{this.props.msg}</p>
+        <p className='subtitle'>Show List.</p>
+        <List title="sample list" data={this.state.list} />
       </div>
     </div>
   }
 }
 
+class List extends Component{
+  number = 1
+
+  render(){
+    let data = this.props.data
+    return <div>
+      <p className='h5 text-center'>{this.props.title}</p>
+      {data.map((item,key)=>
+      <li className='list-group-item' key={key}>
+        <Item number={key + 1} value={item} />
+      </li>)}
+    </div>
+  }
+}
+
+
+class Item extends Component{
+  itm = {
+    fontSize:"16pt",
+    color:"#00f",
+    textDecoration: 'underline',
+    textDecorationColor: '#ddf'
+  }
+  num = {
+    fontWeight:"bold",
+    color:"red"
+  }
+
+  render(){
+    return(
+      <p style={this.itm}>
+        <span style={this.num}>
+          [{this.props.number}]&nbsp;
+        </span>
+        {this.props.value}
+      </p>
+    )
+  }
+
+}
 export default App;
