d9587cba97e74404ef922e5ab8ee6d7c4b30dcee
MissionaryPS <39906446+MissionaryPS@users.noreply.github.com>
2023-01-25 17:48:57 +0900

    4-1 Hooks入門

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,87 +1,52 @@
-import React, {Component} from 'react'
+import React, {useState} from 'react'
 import './App.css';
 
-const theme = {
-  light:{
-    style:{
-      backgroundColor:"#f0f9ff",
-      color:"#00f"
-    },
-    head:"bg-primary text-white display-4 mb-4",
-    alert:"alert alert-primary my-3",
-    text:"text-primary m-3",
-    foot:"py-4"
-  },
-  dark:{
-    style:{
-      backgroundColor:"#336",
-      color:"#eef"
-    },
-    head:"bg-secondary text-white display-4 mb-4",
-    alert:"alert alert-dark my-3",
-    text:"text-light m-3",
-    foot:"py-4"
-  }
-}
 
-const ThemeContext = React.createContext(theme.light)
 
-class App extends Component {
-  static contextType = ThemeContext
+function App() {
+  const [message] = useState("Welcome to Hooks")
 
-  constructor(props){
-    super(props)
-    this.state = {
-      theme:theme.light
-    }
-
-    this.toggleTheme = () =>{
-      this.setState(state =>({
-        theme:
-          state.theme===theme.dark
-          ?theme.light
-          :theme.dark
-      }))
-    }
+  const [count, setCount] = useState(0)
+  const clickFunc = () =>{
+    setCount(count + 1)
   }
 
+  const [flag, setFlag] = useState(false)
+  const changeFlag = (e) => {
+    setFlag(e.target.checked)
+  }
 
-  render(){ 
-    return <div style = {this.context.style}>
-      <h1 className={this.context.head}>React</h1>
-      <div className='container'>
-        <ThemeContext.Provider value={this.state.theme}>
-        <Title value='Content page'/>
-        <Message value='This is Content sample.'/>
-          <Message value='これはテーマのサンプルです．'/>
-        </ThemeContext.Provider>
-        <button onClick={this.toggleTheme}>change theme</button>
-        <div className={this.context.foot}></div>
+  return <div>
+    <h1 className='bg-primary text-white display-4'>React</h1>
+    <div className='container'>
+      <h4 className='my-3'>Hooks sample</h4>
+      {flag?
+        <div className='alert alert-primary text-center'>
+          <p className='h5'>{message}</p>
+          <p className='h5 mb-3'>click : {count} times!</p>
+          <div>
+            <button className='btn btn-primary' onClick={clickFunc}>Click me</button>
+          </div>
+        </div>    
+      :
+        <div className='card p-3 border-primary text-center'>
+          <p className='h5'>{message}</p>
+          <p className='h5 mb-3 text-left text-primary'>click : {count} times!</p>
+          <div>
+            <button className='btn btn-primary' onClick={clickFunc}>Click me</button>
+          </div>
+        </div>
+      }
+      <div className='formgroup h6 text-center pt-3'>
+        <input type='checkbox' className='form-check-input' id='check1' onChange={changeFlag} />
+        <label className='form-check-lbel' htmlFor='check1'>
+          Change form style.
+        </label>
       </div>
     </div>
-  }
+  </div>
 }
 
 
-class Title extends Component{
-  static contextType = ThemeContext
-
-  render(){
-    return <div className={this.context.alert}>
-      <h2>{this.props.value}</h2>
-    </div>
-  }
-}
-
-
-class Message extends Component{
-  static contextType = ThemeContext
-
-  render(){
-    return <div style={this.context.style}>
-      <p className={this.context.text}>{this.props.value}</p>
-    </div>
-  }
-}
 
 export default App;
